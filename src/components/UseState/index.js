e105390640da60d5af2366b119e7e290e2d143c6
Andre Abad <106104291+dreabd@users.noreply.github.com>
2023-04-28 23:05:46 +0000

    Update index.js and index.js

--- src/components/UseState/index.js
+++ src/components/UseState/index.js
@@ -1,18 +1,35 @@
-import './UseState.css';
+import "./UseState.css";
+import { useState } from "react";
 
 const UseState = () => {
+  const [theme, setTheme] = useState("light");
+  const [count, setCount] = useState(0);
+  const [toggle, setToggle] = useState(false);
   return (
-    <div className="state">
+    <div className={`state ${theme}`}>
       <h1>UseState Component</h1>
-      <button>Dark</button>
-      <button>Light</button>
-      <h2>DISPLAY COUNT HERE</h2>
-      <button>
-        Increment
+      <button
+        onClick={() =>
+          setToggle(() => {
+            if (toggle) {
+              setTheme("light");
+              return false;
+            } else {
+              setTheme("dark");
+              return true;
+            }
+          })
+        }
+      >
+        Toggle Theme
       </button>
-      <button>
-        Decrement
-      </button>
+      {/* {toggle && <h1>On</h1>}
+      {!toggle && <h1>Off</h1>} */}
+      <button onClick={() => setTheme("dark")}>Dark</button>
+      <button onClick={() => setTheme("light")}>Light</button>
+      <h2>{count}</h2>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
     </div>
   );
 };
